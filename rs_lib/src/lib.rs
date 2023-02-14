19492a4f2327263141aabd6b06d35bb1b001bab6
Sean Aye <hello@seanaye.ca>
2023-02-13 21:22:09 -0800

    update rust lib

--- rs_lib/src/lib.rs
+++ rs_lib/src/lib.rs
@@ -1,15 +1,39 @@
+use gloo_net::http::{Request, Response};
+use serde::Serialize;
+use std::{collections::HashMap, fmt::Debug};
 use wasm_bindgen::prelude::*;
-use web_sys::{Request, Response, ResponseInit, Headers};
+
+#[derive(Serialize)]
+struct Data {
+  url: String,
+  request_headers: HashMap<String, String>,
+  body: String,
+}
 
 #[wasm_bindgen]
-pub async fn handler(req: Request) -> Result<Response, JsValue> {
-    let a = req.url();
-    let headers = Headers::new()?;
-    headers.append("Content-Type", "application/json")?;
-    let mut init = ResponseInit::new();
-    init.status(200).headers(&headers);
-    let s = format!(r##"{{ "message": "helloworld", "from": "{}"  }}"##, a);
-    Response::new_with_opt_str_and_init(Some(s.as_str()), &init)
+pub async fn handler(
+  req: web_sys::Request,
+) -> Result<web_sys::Response, JsError> {
+  let req: Request = req.into();
+  let request_headers: HashMap<_, _> = req.headers().entries().collect();
+  let text = req.text().await.unwrap_or("".to_string());
+  let d = Data {
+    url: req.url(),
+    request_headers,
+    body: format!(
+      "The request body contained: {}",
+      if text.is_empty() {
+        "nothing".to_string()
+      } else {
+        text
+      }
+    ),
+  };
+
+  Response::new()
+    .json(&d)
+    .map(|x| x.into())
+    .map_err(|x| JsError::new("An error occurred"))
 }
 
 #[cfg(test)]
